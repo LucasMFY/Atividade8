7086c97632a86966abc3e4311e818f47e32733ef
RafaelBertani <Rafaelm.bertani@gmail.com>
2026-08-10 23:45:15 -0300

    feat: adiciona exercicios de 16 a 33

--- src/exercises.js
+++ src/exercises.js
@@ -44,57 +44,101 @@ function b15EncontrarNome(array, busca) {
 }
 
 function b16TemNegativo(array) {
+  return array.some( item => item<0 );
 }
 
 function b17TodosPositivos(array) {
+  return array.every( item => item>0 );
 }
 
 function b18PrimeiroMaiorQue(array, limite) {
+  return array.find( item => item>limite );
 }
 
 function b19ContarPalavras(frase) {
+  return frase.trim().split(/\s+/).length;
 }
 
 function b20Capitalizar(frase) {
+  //return frase.split('')[0].toUpperCase() + frase.slice(1);
+  //return frase.slice(0)[0].toUpperCase() + frase.slice(1);
+  return frase.charAt(0).toUpperCase() + frase.slice(1);
 }
 
 function b21RemoverDuplicados(array){
+  return [...new Set(array)];
 }
 
 function b22CriarMapaDePrecos(produtos) {
+  const mapa = new Map();
+
+  for (const produto of produtos) {
+    mapa.set(produto.nome, produto.preco);
+  }
+
+  return mapa;
 }
 
 function b23PegarValorObjeto(objeto, chave) {
+  return objeto[chave];
 }
 
 function b24SomarValoresObjeto(objeto) {
+  let soma = 0;
+  for (const chave in objeto) {    
+    if (typeof objeto[chave] === "number"){
+      soma+=objeto[chave];
+    }
+  }
+  return soma;
 }
 
 function b25MesDaData(data) {
+  return data.getMonth()+1;
 }
 
 function b26FormatarDataBR(data) {
+  const dia = String(data.getDate() + 1).padStart(2, '0');
+  const mes = String(data.getMonth() + 1).padStart(2, '0');
+  const ano = data.getFullYear();
+
+  return `${dia}/${mes}/${ano}`;
 }
 
 function b27EhBissexto(ano) {
+  return ano % 400 === 0 || (ano % 4 === 0 && ano % 100 !== 0);
 }
 
 function b28UltimoItem(lista) {
+  return lista[lista.length-1];
 }
 
 function b29GerarRange(inicio, fim) {
+  let lista = [];
+  for(let i=inicio; i<=fim; i++){
+    lista[i-inicio] = i;
+  }
+  return lista;
 }
 
 function b30Media(lista) {
+  return lista.reduce( (soma, item) => soma+item )/lista.length;
 }
 
 function b31ClassificarNota(nota) {
+  let status;
+  if(nota>=7){status="aprovado";}
+  else if(nota>=5){status="recuperacao";}
+  else{status="reprovado";}
+  return status;
 }
 
 function b32JuntarNomes(lista) {
+  return lista.join(', ');
 }
 
 function b33ContemPropriedade(objeto, propriedade) {
+  return !(objeto[propriedade] === undefined);
 }
 
 module.exports = {
